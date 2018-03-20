d28cb35bb55514574ada94ff5d7969b036be1074
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2018-03-20 12:25:39 +0100

    norm init clear

--- init.c
+++ init.c
@@ -6,10 +6,9 @@
 /*   By: pfichepo <pfichepo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 10:24:38 by pfichepo          #+#    #+#             */
-/*   Updated: 2018/03/20 12:20:08 by pfichepo         ###   ########.fr       */
+/*   Updated: 2018/03/20 12:25:14 by pfichepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <nm.h>
 
@@ -17,7 +16,7 @@
 ** Put the cmd data at the end of the existing list
 */
 
-void 	mlccmd(t_env *env, uint64_t n_value, char symbol, char *name)
+void	mlccmd(t_env *env, uint64_t n_value, char symbol, char *name)
 {
 	t_cmd *cmd;
 	t_cmd *cmd2;
@@ -38,43 +37,26 @@ void 	mlccmd(t_env *env, uint64_t n_value, char symbol, char *name)
 	cmd2->next = cmd;
 }
 
-void 	init_commands(t_env *env)
+void	init_commands(t_env *env)
 {
 	if ((env->section = (t_lsection*)malloc(sizeof(t_lsection))) == NULL)
-		return;
-	//printf("64 = %i swap = %i fat = %i\n", env->is64bit, env->isswap, env->isfattype);
+		return ;
 	env->section ->first = NULL;
 	env->section ->last = NULL;
-
 	if (env->isarchive)
 	{
 		handle_ar(env->ptr, env->end, env);
-		return;
+		return ;
 	}
 	if (env->is64bit)
-	{
-		if (env->isswap)
-			handle_64(env, env->ptr, env->end, true);
-		else
-			handle_64(env, env->ptr, env->end, false);
-	}
-	else if (!env->isfattype)// 32bits non fat
-	{
-		if (env->isswap) // swapped 32bits
-			handle_32(env, env->ptr, env->end, true);
-		else //32bits
-			handle_32(env, env->ptr, env->end, false);
-	}
-	else // fat
-	{
-		if (env->isswap) // swapped fat
-			handle_fat(env, true);
-		else //fat
-			handle_fat(env, false);
-	}
+		handle_64(env, env->ptr, env->end, env->isswap);
+	else if (!env->isfattype)
+		handle_32(env, env->ptr, env->end, env->isswap);
+	else
+		handle_fat(env, env->isswap);
 }
 
-t_env *make_env(char *ptr, char* end, char* name)
+t_env	*make_env(char *ptr, char *end, char *name)
 {
 	t_env *env;
 
@@ -83,12 +65,16 @@ t_env *make_env(char *ptr, char* end, char* name)
 	env->file_name = name;
 	env->ptr = ptr;
 	env->end = end;
-	env->is64bit = (*(uint32_t*)ptr == MH_MAGIC_64 || *(uint32_t*)ptr == MH_CIGAM_64);
-	env->isswap = (*(uint32_t*)ptr == MH_CIGAM_64 || *(uint32_t*)ptr == MH_CIGAM || *(uint32_t*)ptr == FAT_CIGAM);
-	env->isfattype = (*(uint32_t*)ptr == FAT_CIGAM || *(uint32_t*)ptr == FAT_MAGIC);
+	env->is64bit = (*(uint32_t*)ptr == MH_MAGIC_64 ||
+		*(uint32_t*)ptr == MH_CIGAM_64);
+	env->isswap = (*(uint32_t*)ptr == MH_CIGAM_64 ||
+		*(uint32_t*)ptr == MH_CIGAM || *(uint32_t*)ptr == FAT_CIGAM);
+	env->isfattype = (*(uint32_t*)ptr == FAT_CIGAM ||
+		*(uint32_t*)ptr == FAT_MAGIC);
 	env->isarchive = check_ar_header(ptr);
 	env->in_ppc = false;
-	if (!env->isarchive && !env->is64bit && !env->isswap && !env->isfattype && *(uint32_t*)ptr != MH_MAGIC)
+	if (!env->isarchive && !env->is64bit && !env->isswap &&
+		!env->isfattype && *(uint32_t*)ptr != MH_MAGIC)
 		failmessage("File not recognized");
 	env->list = NULL;
 	init_commands(env);
