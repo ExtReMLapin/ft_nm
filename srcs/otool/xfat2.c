3afc9b7bb76811f007506bba087ef653c88da832
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2018-03-27 12:45:37 +0200

    norm

--- srcs/otool/xfat2.c
+++ srcs/otool/xfat2.c
@@ -6,7 +6,7 @@
 /*   By: pfichepo <pfichepo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/20 09:36:17 by pfichepo          #+#    #+#             */
-/*   Updated: 2018/03/27 11:42:57 by pfichepo         ###   ########.fr       */
+/*   Updated: 2018/03/27 12:43:09 by pfichepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,26 @@ bool		shouldprintcpu(struct fat_arch *c, struct fat_arch *arch, uint32_t n)
 	return (true);
 }
 
+bool check_var_cpu(cpu_type_t c, bool one, bool current)
+{
+	if (one)
+	{
+		if (c == CPU_TYPE_X86 || c == CPU_TYPE_I386 ||
+			swap_uint32(c) == CPU_TYPE_X86 || swap_uint32(c) == CPU_TYPE_I386)
+		{
+			return (true);
+		}
+		return (current);
+	}
+	else
+	{
+		if (c == CPU_TYPE_X86_64 || swap_uint32(c) == CPU_TYPE_X86_64)
+			return (true);
+		else
+			return (current);
+	}
+}
+
 uint32_t	how_many_cpu(struct fat_arch *a, uint32_t n)
 {
 	bool			has_i386;
@@ -86,18 +106,14 @@ uint32_t	how_many_cpu(struct fat_arch *a, uint32_t n)
 	while (n--)
 	{
 		c = a->cputype;
-		if (c == CPU_TYPE_X86 || c == CPU_TYPE_I386 ||
-			swap_uint32(c) == CPU_TYPE_X86 || swap_uint32(c) == CPU_TYPE_I386)
+		has_i386 = check_var_cpu(c, true, has_i386);
+		has_x86_x64 = check_var_cpu(c, false, has_x86_x64);
+		if ((get_cputype(c)[0] != '?' || get_cputype(swap_uint32(c))[0] != '?')
+			&& firstfoundcpu(original, c, noriginal) == a)
 		{
-			has_i386 = true;
-		}
-		if (c == CPU_TYPE_X86_64 || swap_uint32(c) == CPU_TYPE_X86_64)
-			has_x86_x64 = true;
-		if ((get_cputype(c)[0] != '?' || get_cputype(swap_uint32(c))[0] != '?') && firstfoundcpu(original, c, noriginal) == a)
 			count++;
+		}
 		a = (void*)a + sizeof(struct fat_arch);
 	}
-	if (has_x86_x64 && has_i386)
-		count--;
-	return (count);
+	return ((has_x86_x64 && has_i386) ? count - 1 : count);
 }
